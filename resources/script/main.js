94e89bff1d7b0b8c9d4921951512c5c448784a1d
Agiothyella <agiothyella@gmail.com>
2021-08-07 22:55:54 +0700

    Add: Our pick section slider
    + Add slide js manual navigation for our picks section

--- resources/script/main.js
+++ resources/script/main.js
@@ -4,13 +4,31 @@ const promotionsSplide = new Splide(".promotions__splide", {
   type: "fade",
   rewind: true,
   cover: true,
-  perPage: 1,
-  gap: 0,
-  padding: 0,
-  width: "100vw",
   height: "43rem",
   autoplay: true,
   interval: 10000,
   pauseOnHover: false,
   pauseOnFocus: false,
 }).mount();
+
+const picksSplide = new Splide(".picks__splide", {
+  type: "fade",
+  perPage: 1,
+  gap: 0,
+  padding: 0,
+  height: "43rem",
+  pagination: false,
+  arrows: false,
+});
+
+const picksSplideNav = new Splide(".picks__splide-nav", {
+  perPage: 2,
+  isNavigation: true,
+  pagination: false,
+  arrows: false,
+  direction: "ttb",
+  height: "5rem",
+  autoWidth: true,
+}).mount();
+
+picksSplide.sync(picksSplideNav).mount();
