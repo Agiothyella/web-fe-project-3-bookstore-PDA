74c6ff95b6520e63085efa5f3663a388cbd1945f
Agiothyella <agiothyella@gmail.com>
2021-08-09 18:32:16 +0700

    Add: Top section
    + Add slide js grid slider for top section

--- resources/script/main.js
+++ resources/script/main.js
@@ -21,6 +21,7 @@ const picksSplide = new Splide(".picks__splide", {
   arrows: false,
   drag: false,
 });
+
 const picksSplideNav = new Splide(".picks__splide-nav", {
   perPage: 2,
   isNavigation: true,
@@ -30,7 +31,68 @@ const picksSplideNav = new Splide(".picks__splide-nav", {
   height: "5rem",
   autoWidth: true,
 }).mount();
+
 picksSplide.sync(picksSplideNav).mount();
+
+// const newSplide = new Splide(".new__splide", {
+//   type: "loop",
+//   rewind: true,
+//   height: "60rem",
+// }).mount();
+
+const topSplide = new Splide(".top__splide", {
+  type: "loop",
+  rewind: true,
+  perPage: 2,
+  perMove: 2,
+  pagination: false,
+  width: "100%",
+  autoHeight: true,
+  grid: {
+    dimensions: [
+      [1, 1],
+      [2, 3],
+      [1, 1],
+      [2, 3],
+    ],
+    gap: {
+      row: "1rem",
+      col: "5rem",
+    },
+  },
+}).mount(window.splide.Extensions);
+
+const seriesSplide = new Splide(".series__splide", {
+  type: "loop",
+  rewind: true,
+  perPage: 1,
+  perMove: 1,
+  autoHeight: true,
+  pagination: false,
+  arrows: false,
+  cover: true,
+  gap: "5rem",
+  speed: 600,
+  padding: {
+    right: "30rem",
+    left: "30rem",
+  },
+});
+
+const seriesSplideText = new Splide(".series__splide-text", {
+  type: "loop",
+  rewind: true,
+  perPage: 1,
+  perMove: 1,
+  autoHeight: true,
+  width: "80rem",
+  gap: 0,
+  padding: 0,
+  pagination: false,
+  speed: 600,
+}).mount();
+
+seriesSplide.sync(seriesSplideText).mount();
 
 const comicSplide = new Splide(".comic__splide", {
   type: "loop",
@@ -40,7 +102,7 @@ const comicSplide = new Splide(".comic__splide", {
   gap: "3rem",
   pagination: false,
   width: "100%",
-  height: "30rem",
+  autoHeight: true,
   padding: {
     left: "2rem",
     right: "2rem",
